7394466a712120b082bfdbd758e39e3497df972a
github username <jrgreiner115@gmail.com>
2018-05-14 16:19:57 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution in this file
+// drivers = [d1, d2, d3, etc]
+// 'd1'
+function findMatching(drivers, string) {
+  return drivers.filter(function(driver) {return string.toLowerCase() === driver.toLowerCase()});
+}
+
+function fuzzyMatch(drivers, string) {
+  return drivers.filter(function(driver) {return string.toLowerCase().charAt(0) === driver.toLowerCase().charAt(0)});
+}
+
+function matchName(drivers, string) {
+  return drivers.filter(function(driver) {return string.toLowerCase() === driver.name.toLowerCase()});
+}
